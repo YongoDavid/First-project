b28e565f41243cfd07496833b5a4f1cd1a4b4bb7
Yongo David <davidyongo9@gmail.com>
2024-02-17 15:59:17 +0100

    routing test

--- server/app.js
+++ server/app.js
@@ -10,12 +10,12 @@ app.use((req,res, next)=>{
     next()
 });
 app.use(morgan('dev'));
-app.use(express.static('client'));
+app.use(express.static('../client'));
 
 app.get('/' , (req,res)=>{
     // res.sendFile(path.join(__dirname , '../client/index.html'));
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
-// app.use((req,res)=>{
-    
-// });
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname , '../client/404.html'));
+});
